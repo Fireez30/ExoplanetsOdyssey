3ca9ae2f532a7ffbb28771ae66b444c064682168
Fireez <benjamin.gallean@etu.umontpellier.fr>
2018-10-26 12:45:38 +0200

    TileList + Poser tile

--- ExoplanetsOdyssey/Assets/Scripts/TileModif.cs
+++ ExoplanetsOdyssey/Assets/Scripts/TileModif.cs
@@ -7,6 +7,8 @@ public class TileModif : MonoBehaviour {
 
     public Tilemap tilemap;
     public Camera cam;
+    public List<TileBase> tileList;
+    public int currentIndex;
 
 	void Update () {
 		if (Input.GetMouseButton(0))
@@ -18,6 +20,37 @@ public class TileModif : MonoBehaviour {
             if (tilemap.GetTile(tilePos))
             {
                 tilemap.SetTile(tilePos, null);
+            }
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            Vector3 screenPos = cam.ScreenToWorldPoint(mousePos);
+            Vector3Int tilePos = tilemap.WorldToCell(screenPos);
+
+            if (!tilemap.GetTile(tilePos))
+            {
+                tilemap.SetTile(tilePos, tileList[currentIndex]);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentIndex != 0)
+            {
+                currentIndex--;
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentIndex < tileList.Count - 1)
+            {
+                currentIndex++;
+            }
+            else
+            {
+                currentIndex = 0;
             }
         }
 	}
