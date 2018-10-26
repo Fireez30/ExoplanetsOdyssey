da2e8b83dbd3b6b136ea685f4dc214eb93a8bfe8
Fireez <benjamin.gallean@etu.umontpellier.fr>
2018-10-26 12:23:34 +0200

    Tile deletion + gitignore

---
+++ ExoplanetsOdyssey/Assets/Scripts/TileModif.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileModif : MonoBehaviour {
+
+    public Tilemap tilemap;
+    public Camera cam;
+
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            Vector3 screenPos = cam.ScreenToWorldPoint(mousePos);
+            Vector3Int tilePos = tilemap.WorldToCell(screenPos);
+
+            if (tilemap.GetTile(tilePos))
+            {
+                tilemap.SetTile(tilePos, null);
+            }
+        }
+	}
+}
