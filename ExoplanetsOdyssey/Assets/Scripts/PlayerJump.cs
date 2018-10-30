8438867e564226fca56799e137d9f10c7585f52c
Benoit Dorey <benoitdorey@gmail.com>
2018-10-30 18:13:54 +0100

    Player move and jump
    Script pour bouger et sauter, plus on reste appuyé sur space plus le saut est long

---
+++ ExoplanetsOdyssey/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	[Range(1,10)]
+	public float jumpVelocity;
+	public float fallMult = 2.5f;
+	public float lowMult = 2f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( Input.GetButtonDown("Jump") && rb.velocity.y == 0 ) 
+		{
+			rb.velocity = Vector2.up * jumpVelocity;
+		}
+
+		if( rb.velocity.y < 0 )
+		{
+			rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMult - 1) * Time.deltaTime; 
+		}
+		else if( rb.velocity.y > 0 && !Input.GetButton("Jump") )
+		{
+			rb.velocity += Vector2.up * Physics2D.gravity.y * (lowMult - 1) * Time.deltaTime; 
+		}
+	}
+}
---
+++ ExoplanetsOdyssey/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float speed = 10.0f;
+	bool facingRight = true;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector2 (speed * move, rb.velocity.y);
+
+		if( rb.velocity.x > 0 && !facingRight )
+		{
+			Flip();
+		}
+		else if( rb.velocity.x < 0 && facingRight )
+		{
+			Flip();
+		}
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+
+		Vector3 scale = transform.localScale;
+
+		scale.x *= -1;
+
+		transform.localScale = scale;
+	}
+	
+}
