f6480371dd31d282a614ab1362cac7bc539e337c
JulienJandot <julienjandot@orange.fr>
2018-10-31 09:11:20 +0100

    first shot, spaceship ui

---
+++ ExoplanetsOdyssey/Assets/Scripts/Spaceship_Planet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spaceship_Planet : MonoBehaviour {
+
+    private Text info;
+
+    [SerializeField]
+    private string infoPlaceholder = "";
+
+
+    void Start () {
+        info = GameObject.Find("Canvas").GetComponentInChildren<Text>();
+        if (info == null)
+            Debug.LogError("Texte non trouvé");
+    }
+	
+	void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0))
+            info.text = infoPlaceholder;
+    }
+}
