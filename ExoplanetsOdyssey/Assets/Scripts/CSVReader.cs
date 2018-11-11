14041b4bafff57414bf1922d518095755ea5a111
JulienJandot <julienjandot@orange.fr>
2018-11-11 16:48:00 +0100

    csv reader, planetInfoplaceholder

---
+++ ExoplanetsOdyssey/Assets/Scripts/CSVReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVReader : MonoBehaviour {
+
+    public TextAsset csvFile;
+
+    private string[] infos;
+
+	void Start () {
+        infos = csvFile.text.Split('\n');
+	}
+	
+	public string getPlanetInfo()
+    {
+        int r = Random.Range(0, infos.Length);
+        return infos[r];
+    }
+}
--- ExoplanetsOdyssey/Assets/Scripts/Spaceship_Planet.cs
+++ ExoplanetsOdyssey/Assets/Scripts/Spaceship_Planet.cs
@@ -10,6 +10,7 @@ public class Spaceship_Planet : MonoBehaviour {
     private Text info;
     private Parameters param;                   //Script dans le GameManager
     private int indexPlanet;
+    private CSVReader csvReader;
 
     [SerializeField]                           //A enlever parce que ce sera généré aléatoirement et pas défini par nous (juste le serialize)
     private string infoPlaceholder = "";
@@ -17,21 +18,24 @@ public class Spaceship_Planet : MonoBehaviour {
 
     void Awake () {
         param = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Parameters>();
-        info = GameObject.Find("Canvas").GetComponentInChildren<Text>();    
+        info = GameObject.Find("Canvas").GetComponentInChildren<Text>();
+        csvReader = GameObject.Find("CSVReader").GetComponent<CSVReader>();
         if (info == null)
             Debug.LogError("Texte non trouvé");
     }
-	
-	void OnMouseOver() {
-        if (Input.GetMouseButtonDown(0))        //<--- A placer dans le mouseDown
-            info.text = infoPlaceholder;
-    }
-    private void OnMouseDown()
+
+    void Start()
     {
+        infoPlaceholder = csvReader.getPlanetInfo();
+    }
+
+    private void OnMouseDown() {
+        info.text = infoPlaceholder;
         Debug.Log("Du vaisseau à la planète");
         param.setCurrentPlanet(indexPlanet);
         SceneManager.LoadScene(2);
     }
+
     public void setIndexPlanet(int i) {
         indexPlanet = i;
     }
