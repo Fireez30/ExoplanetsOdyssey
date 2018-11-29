0f17eca9d582387fcc50dafb03fcfb55ebf94282
Fireez <benjamin.gallean@etu.umontpellier.fr>
2018-11-29 08:56:39 +0100

    Choix user, scene vide game over, avancée musique benji

---
+++ ExoplanetsOdyssey/Assets/ChoiceWindow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceWindow : MonoBehaviour {
+    public GameObject fenetrechoix;
+    bool active = false;
+    int actual;
+
+    public void showChoiceWindow()
+    {
+        if (!active)
+            active = true;
+        else
+            active = false;
+
+        fenetrechoix.SetActive(active);
+
+    }
+
+    public void SetChoice(int i)
+    {
+        actual = i;
+    }
+
+    public void SendChoice()
+    {
+        if (actual != -1)
+            GameObject.FindGameObjectWithTag("GameManager").GetComponent<UserChoices>().addChoice(actual);
+        showChoiceWindow();
+    }
+}
+
---
+++ ExoplanetsOdyssey/Assets/UserChoices.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserChoices : MonoBehaviour
+{
+
+    public List<Choice> choices;
+
+    // Use this for initialization
+    void Awake()
+    {
+        choices = new List<Choice>();
+    }
+
+    public void addChoice(int planet)
+    {
+        foreach (Choice c in choices)//Cycle through already made choice
+        {
+            if (c.systemIndex == this.gameObject.GetComponent<Parameters>().currentSystem && c.planetIndex == planet)//if choice already done
+                return;//do nothing
+        }
+        Choice c2 = new Choice();
+        c2.systemIndex = this.gameObject.GetComponent<Parameters>().currentSystem;
+        c2.planetIndex = planet;
+        choices.Add(c2);//add the actual planet as a choice
+    }
+
+    public void removeChoice(int index)
+    {
+        if (!choices[index].Equals(null))
+        {
+            choices.RemoveAt(index);
+        }
+    }
+}
+
+[System.Serializable]
+public struct Choice
+{
+    public int systemIndex;
+    public int planetIndex;
+}
