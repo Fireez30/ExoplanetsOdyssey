af1b48150824ea3e8d50bfecf014753eab1960b9
Fireez <benjamin.gallean@etu.umontpellier.fr>
2018-12-01 10:58:15 +0100

    Affichage carburant scene universe + debug

---
+++ ExoplanetsOdyssey/Assets/Scripts/FuelDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelDisplay : MonoBehaviour {
+    public Text t;
+
+	// Use this for initialization
+	void Awake () {
+        int fuel = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ShipInventory>().fuelAmount;
+        t.text = "Carburant : " + fuel;
+        t.color = Color.white;
+	}
+}
