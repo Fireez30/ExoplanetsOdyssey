290f20e71463d3e7190587d4cd578e04d7513879
Fireez <benjamin.gallean@etu.umontpellier.fr>
2018-12-12 10:03:14 +0100

    sprites classic, main menu dégueu, débugs , UI, Grandient sky

---
+++ ExoplanetsOdyssey/Assets/Scripts/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadScene () {
+        //Setup GameManager
+        SceneManager.LoadScene(0);//load universe
+	}
+	
+}
