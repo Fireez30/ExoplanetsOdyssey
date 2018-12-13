684ebc08350ef20db81d41fe00e327c12683c6aa
Fireez <benjamin.gallean@etu.umontpellier.fr>
2018-12-13 11:07:44 +0100

    Refonte UI dans le vaisseau

---
+++ ExoplanetsOdyssey/Assets/Scripts/UpdateShipUIState.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UpdateShipUIState : MonoBehaviour {
+
+    public Image fuel;
+    public Image oxygen;
+    public Image scanner;
+
+    public Sprite red;
+    public Sprite green;
+
+    ShipInventory p;
+	// Use this for initialization
+
+	void Awake () {
+        p = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ShipInventory>();
+
+        if (p.GetFuelTankState() == 0 && fuel.sprite != red)
+        {
+            fuel.sprite = red;
+        }
+        else if (p.GetFuelTankState() == 1 && fuel.sprite != green)
+        {
+            fuel.sprite = green;
+        }
+
+        if (p.GetOxygenTankState() == 0 && oxygen.sprite != red)
+        {
+            oxygen.sprite = red;
+        }
+        else if (p.GetOxygenTankState() == 1 && oxygen.sprite != green)
+        {
+            oxygen.sprite = green;
+        }
+
+        if (p.GetScannerState() == 0 && scanner.sprite != red)
+        {
+            scanner.sprite = red;
+        }
+        else if (p.GetScannerState() == 1 && scanner.sprite != green)
+        {
+            scanner.sprite = green;
+        }
+
+    }
+}
