57b235b4a683f5affbd0b7ee0c5a0b237ea82b79
Benoit Dorey <benoitdorey@gmail.com>
2018-12-18 16:08:33 +0100

    Iron display

---
+++ ExoplanetsOdyssey/Assets/Scripts/IronDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IronDisplay : MonoBehaviour
+{
+    public Text t;
+
+    // Use this for initialization
+    void Awake()
+    {
+        int iron = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ShipInventory>().ironAmount;
+        t.text = "Fer : " + iron;
+        t.color = Color.white;
+    }
+}
