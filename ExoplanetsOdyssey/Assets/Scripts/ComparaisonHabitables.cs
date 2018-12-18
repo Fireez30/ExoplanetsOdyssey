a418a897dc3fd3edbab978a58f0cda7ba4dcbb6a
Foopyo <romain.chamoulaud@gmail.com>
2018-12-18 15:08:06 +0100

    Debug fin

--- ExoplanetsOdyssey/Assets/Scripts/ComparaisonHabitables.cs
+++ ExoplanetsOdyssey/Assets/Scripts/ComparaisonHabitables.cs
@@ -35,13 +35,13 @@ public class ComparaisonHabitables : MonoBehaviour {
     void Update()
     {
         //scroll
-        t.transform.position = new Vector2(t.transform.position.x, t.transform.position.y + 0.5f);
+        t.transform.position = new Vector2(t.transform.position.x, t.transform.position.y+0.005f);
     }
 
     void ComputeLines()
     {
-        lines.Add("Félicitations vous avez choisi les " + planetNumber + " planètes que vous considérez habitables.");
-        lines.Add("Voyons maintenant ce qui va ou ne va pas dans vos choix :");
+        lines.Add("Félicitation vous avez choisi les " + planetNumber + " planètes que vous considérez habitables.\n");
+        lines.Add("Voyons maintenant ce qui va ou ne va pas dans vos choix :\n");
         foreach (Choice c in choices)
         {
             bool flag = false;
@@ -56,37 +56,39 @@ public class ComparaisonHabitables : MonoBehaviour {
 
             if (flag)
             {
-                lines.Add("La planète " + (choices.IndexOf(c) + 1) + " du système " + c.systemName + " est en effet une planète habitable");
+                lines.Add("La planète " + (choices.IndexOf(c) + 1) + " du système " + c.systemName + " est en effet une planète habitable\n");
                 nbCorrects++;
             }
             else
             {
-                string s = "La planète " + (choices.IndexOf(c) + 1) + " du système " + c.systemName + " n'est pas une planète habitable, pour les raisons suivantes : ";
+                string s = "La planète " + (choices.IndexOf(c) + 1) + " du système " + c.systemName + " n'est pas une planète habitable, pour les raisons suivantes : \n";
 
                 int seed = param.getSeed(c.systemIndex, c.planetIndex);
                 string[] infos = param.getInfo(seed).Split(',');
 
                 if (infos[0] == "Gazeuse")
-                    s += "elle est gazeuse \n";
+                    s += "- C'est une planète gazeuse \n";
                 if (0 > Int32.Parse(infos[1]))
-                    s += "elle est trop froide : " + infos[1] + "°\n";
+                    s += "- La planète est trop froide : " + infos[1] + "°\n";
                 else if (80 < Int32.Parse(infos[1]))
-                    s += "elle est trop chaude : " + infos[1] + "°\n";
+                    s += "- La planète est trop chaude : " + infos[1] + "°\n";
 
                 if (Int32.Parse(infos[2].Split('^')[1]) == 20)
-                    s += "elle est trop légère\n";
+                    s += "- La planète est trop légère\n";
                 else if (Int32.Parse(infos[2].Split('^')[1]) <= 24 && Double.Parse(infos[2].Split('^')[0].Split('*')[0]) < 2.5)
-                    s += "elle est trop légère";
+                    s += "- La planète est trop légère\n";
                 else if (Int32.Parse(infos[2].Split('^')[1]) == 28)
-                    s += "elle est trop lourde\n";
+                    s += "- La planète est trop lourde\n";
                 if (infos[3].Contains("pas"))
-                    s += "il n'y a pas d'atmosphère\n";
-                if (!infos[4].Contains("calme"))
-                    s += infos[4];
+                    s += "- La planète n'a pas d'atmosphère\n";
+                /*if (!infos[4].Contains("calme"))
+                    s += infos[4];*/
+
+
                 lines.Add(s);
             }
         }
-        lines.Add("Au final vous avez fais " + nbCorrects + " choix corrects sur " + planetNumber);
+        lines.Add("Vous avez fais " + nbCorrects + " choix corrects sur " + planetNumber + "\n");
 
         float pourcentage = (float)nbCorrects / (float)planetNumber;
 
@@ -95,7 +97,7 @@ public class ComparaisonHabitables : MonoBehaviour {
         else if (pourcentage <= 0.7f && pourcentage > 0.3f)
             lines.Add("Bravo, tu as résolu certains des mystères de l'espace, même si certains restent encore à découvrir !");
         else
-            lines.Add("Félicitations, les planètes n'ont plus de secrets pour toi !");
+            lines.Add("Félicitation, les planètes n'ont plus de secrets pour toi !");
 
     }
 }
