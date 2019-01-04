64d6789a8c6ea47ad4634be8f2cf175c8a096408
Fireez <benjamin.gallean@etu.umontpellier.fr>
2019-01-04 15:39:58 +0100

    Ajout Events music planete

---
+++ ExoplanetsOdyssey/Assets/Scripts/MusicControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControl : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string Music = "event:/Music/Planet";
+
+	private int Rand;
+	public int diceCheck;
+    FMOD.Studio.EventInstance musicEv;
+    
+	// Use this for initialization
+	void Start () {
+		musicEv = FMODUnity.RuntimeManager.CreateInstance(Music);
+		musicEv.start();
+		Rand = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Random.Range(0, 100) > diceCheck)
+		{
+			Rand = Random.Range(0, 100);
+			SendRandomChangeToFMOD();
+		}
+	}
+
+	public void SendRandomChangeToFMOD()
+	{
+		musicEv.setParameterValue("Random", Rand);
+	}
+}
